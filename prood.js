fbf49f4efd350fb4fe79a63d123ccf9c6598276b
Stepan <stepan93rozhko@gmail.com>
2020-03-03 11:02:05 +0200

    'document_reed'

--- prood.js
+++ prood.js
@@ -1,25 +1,34 @@
+$( document ).ready(function() {
+ 
+    var header = ".header";
+    var block = ".block";
+    var no = ".no";
+    var yes = ".yes";
 
+    $("#headerButton").click(function(){
+        $(header).hide();
+        $(block).show();
+    });
+
+    $(".block__no").click(function(){
+        $(block).hide();
+        $(no).show();
+    });
+
+    $(".no__out").click(function(){
+        $(no).hide();
+        $(header).show();
+    });
+
+    $(".block__yes").click(function(){
+        $(block).hide();
+        $(yes).show();
+    });
+
+    
+});
    
 
- $("#headerButton").click(function(){
-    $("#header").hide();
-    $("#block").show();
-});
-
-$(".block__no").click(function(){
-    $("#block").hide();
-    $(".no").show();
-});
-
-$(".no__out").click(function(){
-    $(".no").hide();
-    $("#header").show();
-});
-
-$(".block__yes").click(function(){
-    $("#block").hide();
-    $(".yes").show();
-});
 
 // $(".yes__form-button").click(function(){
 //     $(".yes").hide();
